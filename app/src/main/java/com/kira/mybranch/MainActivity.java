7d40cb0bb7cd01bf08f6d518923bff556fe169e6
zhangmingyang <zhangmingyang1@hz41319.com>
2019-06-23 18:12:56 +0800

    增加second
    (cherry picked from commit f0a0892)

--- app/src/main/java/com/kira/mybranch/MainActivity.java
+++ app/src/main/java/com/kira/mybranch/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test分支第一次提交
+        //test第二次 修改
     }
 }
